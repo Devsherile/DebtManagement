99a5913a8274b1cc3c6c45b17f0bb9918b1f8008
Sherile <sherilecheptoo1@gmail.com>
2024-03-23 23:37:34 +0300

    fix

--- src/pages/authentication/Register.jsx
+++ src/pages/authentication/Register.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-
+import "./Register.css";
 function Register() {
   return (
-    <div>
+    <div id="register">
       <div className="register">
-      <h1>Client Panel</h1>
-      <h3>Register</h3>
+        <h1>Client Panel</h1>
+        <h3>Register</h3>
       </div>
       <div className="label">
-      <input type="text" />
-      <label>Email</label>
-      <input type="password" />
-      <label>Password</label>
+        <input type="text" />
+        <label>Email</label>
+        <input type="password" />
+        <label>Password</label>
+        <button>Register</button>
       </div>
     </div>
   );
